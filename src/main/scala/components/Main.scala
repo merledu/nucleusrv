f21267434e1b65463c35c86a559693da1ff560f8
Usman <usmanzain95@gmail.com>
2021-09-15 18:39:44 +0500

    added main class and c tb

--- src/main/scala/components/Main.scala
+++ src/main/scala/components/Main.scala
@@ -1,5 +1,14 @@
 package components
+import java.io.{File, FileWriter}
 
-object Main {
+object Main extends App {
+  val dir = new File(args(0)) ; dir.mkdirs
+  val chirrtl = firrtl.Parser.parse(chisel3.Driver.emit(() => new CPU()))
+  val writer = new FileWriter(new File(dir, s"${chirrtl.main}.fir"))
+  writer write chirrtl.serialize
+  writer.close()
 
+  val verilog = new FileWriter(new File(dir, s"${chirrtl.main}.v"))
+  verilog write (new firrtl.VerilogCompiler).compileAndEmit(firrtl.CircuitState(chirrtl, firrtl.ChirrtlForm)).getEmittedCircuit.value
+  verilog.close()
 }
