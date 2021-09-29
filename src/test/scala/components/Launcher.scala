708a097538c9c23c6775c8e3953e5f883ba18cec
shahzaib <shahzaibceo@gmail.com>
2021-09-29 15:11:08 +0500

    I/O testers off

--- src/test/scala/components/Launcher.scala
+++ src/test/scala/components/Launcher.scala
@@ -1,19 +1,19 @@
 
-package components
+// package components
 
-import chisel3.iotesters.{Driver, TesterOptionsManager}
-import utils.TutorialRunner
-import chisel3._
+// import chisel3.iotesters.{Driver, TesterOptionsManager}
+// import utils.TutorialRunner
+// import chisel3._
 
-object Launcher {
-  val tests = Map(
-    "cpu" -> {(manager: TesterOptionsManager)=>
-      Driver.execute(()=>new CPU(), manager){
-        (c)=>new RiscTest(c)
-      }
-    }
-  )
-  def main(args: Array[String]): Unit = {
-    TutorialRunner("tests", tests, args)
-  }
-}
+// object Launcher {
+//   val tests = Map(
+//     "cpu" -> {(manager: TesterOptionsManager)=>
+//       Driver.execute(()=>new CPU(), manager){
+//         (c)=>new RiscTest(c)
+//       }
+//     }
+//   )
+//   def main(args: Array[String]): Unit = {
+//     TutorialRunner("tests", tests, args)
+//   }
+// }
