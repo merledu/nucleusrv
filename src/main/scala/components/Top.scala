2386bbe66e292206518496cc138997c8fc2edbe0
Usman <usmanzain95@gmail.com>
2021-09-30 12:32:08 +0500

    added top wrapper, instantiates memory

--- src/main/scala/components/Top.scala
+++ src/main/scala/components/Top.scala
@@ -1,11 +1,19 @@
 package components
 import chisel3._
 
-class CpuIO extends Bundle {
-  val w_enable = Input(UInt(32.W))
-}
+
 
 class Top extends Module{
-  val io = IO(new CpuIO())
+  val io = IO(new Bundle() {
+    val pin = Output(UInt(32.W))
+  })
+
+  val imem: InstructionMemory = Module(new InstructionMemory)
+  val dmem: DataMemory = Module(new DataMemory)
+  val core: Core = Module(new Core)
+
+  core.io.imem <> imem.io
+  core.io.dmem <> dmem.io
+  io.pin := core.io.pin
 
 }
