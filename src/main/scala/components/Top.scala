5eefb50b991a163b432049f0570be2906dd6b5a9
Usman <usmanzain95@gmail.com>
2021-10-01 18:54:07 +0500

    work in progress

--- src/main/scala/components/Top.scala
+++ src/main/scala/components/Top.scala
@@ -1,19 +1,19 @@
+
 package components
 import chisel3._
+import caravan.bus.wishbone.{WishboneConfig, WBRequest, WBResponse}
 
-
-
-class Top extends Module{
+class Top (implicit val config:WishboneConfig) extends Module{
   val io = IO(new Bundle() {
     val pin = Output(UInt(32.W))
   })
 
-  val imem: InstructionMemory = Module(new InstructionMemory)
-  val dmem: DataMemory = Module(new DataMemory)
-  val core: Core = Module(new Core)
+//  val imem: InstructionMemory = Module(new InstructionMemory)
+//  val dmem: DataMemory = Module(new DataMemory)
+  val core: Core = Module(new Core(new WBRequest, new WBResponse))
 
-  core.io.imem <> imem.io
-  core.io.dmem <> dmem.io
+//  core.io.imem <> imem.io
+//  core.io.dmem <> dmem.io
   io.pin := core.io.pin
 
 }
