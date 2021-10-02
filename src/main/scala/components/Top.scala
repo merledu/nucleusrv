c3bf21a18be72ef5071f5dd0dfd9edb0f2b2dca1
shahzaib <shahzaibceo@gmail.com>
2021-10-02 16:33:36 +0500

    Top File added

--- src/main/scala/components/Top.scala
+++ src/main/scala/components/Top.scala
@@ -1,23 +1,23 @@
-
 package components
 import chisel3._
 import caravan.bus.common.{BusConfig, AbstrRequest, AbstrResponse, BusHost, BusDevice}
+import caravan.bus.wishbone.{WishboneConfig, WBRequest, WBResponse, WishboneAdapter}
 import jigsaw.rams.fpga.BlockRam
 
-class Top(val req:AbstrRequest, val rsp:AbstrResponse,val adapter:Module , programFile:Option[String]) (implicit val config:BusConfig) extends Module{
+class Top(/*val req:AbstrRequest, val rsp:AbstrResponse,val instrAdapter:Module, val dataAdapter:Module ,*/ programFile:Option[String]) (implicit val config:BusConfig) extends Module{
   val io = IO(new Bundle() {
     val pin = Output(UInt(32.W))
   })
 
 //  val imem: InstructionMemory = Module(new InstructionMemory)
 //  val dmem: DataMemory = Module(new DataMemory)
-  val core: Core = Module(new Core(req, rsp))
-  val imemAdapter = Module(new adapter())
-  val dmemAdapter = Module(new adapter())
-  
+  val core: Core = Module(new Core(/*req, rsp*/ new WBRequest,new WBResponse()))
+  val imemAdapter = Module(new WishboneAdapter()) //instrAdapter
+  val dmemAdapter = Module(new WishboneAdapter())
+
   // TODO: Make RAMs generic
-  val imemCtrl = Module(new BlockRam.createNonMaskableRAM(programFile, config, 1024))
-  val dmemCtrl = Module(new BlockRam.createMaskableRAM(config, 1024))
+  val imemCtrl = Module(BlockRam.createNonMaskableRAM(programFile, config, 1024))
+  val dmemCtrl = Module(BlockRam.createMaskableRAM(config, 1024))
 
   /*  Imem Interceonnections  */
   imemAdapter.io.reqIn <> core.io.imemReq
@@ -30,7 +30,6 @@ class Top(val req:AbstrRequest, val rsp:AbstrResponse,val adapter:Module , progr
   core.io.dmemRsp <> dmemAdapter.io.rspOut
   dmemCtrl.io.req <> dmemAdapter.io.reqOut
   dmemAdapter.io.rspIn <> dmemCtrl.io.rsp
-
 
 //  core.io.imem <> imem.io
 //  core.io.dmem <> dmem.io
