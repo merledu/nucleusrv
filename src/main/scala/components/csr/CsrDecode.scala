325d4b9f505df98a972cb628aac1ed133afc7672
Usman <usmanzain95@gmail.com>
2021-11-02 16:55:33 +0500

    added csr regfile and decode logic

---
+++ src/main/scala/components/csr/CsrDecode.scala
@@ -0,0 +1,33 @@
+package nucleusrv.components.csr
+import chisel3._
+import chisel3.util._
+
+
+class CsrDecode extends Module {
+  val io = IO(new Bundle{
+    val func3 = Input(UInt(3.W))
+
+    val csr_cmd = Output(UInt(2.W))
+    val csr_in = Output(UInt(1.W))
+  })
+
+  val W = BitPat("b?01") //CSSRW
+  val S = BitPat("b?10") //CSSRS
+  val C = BitPat("b?11") //CSSRC
+
+
+  //when msb of funct3 = 1, instructions are of immediate type
+  when(io.func3 === BitPat("b1??")){
+    io.csr_in := 1.U // immediate value
+  }.elsewhen(io.func3 === BitPat("b0??")){
+    io.csr_in := 0.U // register value
+  }
+
+  when(io.func3 === W){
+    io.csr_cmd := 1.U
+  }.elsewhen(io.func3 === S){
+    io.csr_cmd := 2.U
+  }.elsewhen(io.func3 === C){
+    io.csr_cmd := 3.U
+  }
+}
--- src/main/scala/components/csr/CsrRegisterFile.scala
+++ src/main/scala/components/csr/CsrRegisterFile.scala
@@ -3,6 +3,21 @@ import chisel3._
 
 class CsrRegisterFile extends Module {
   val io = IO(new Bundle {
-
+    val csr_addr = Input(UInt(12.W))
+    val rs1_addr = Input(UInt(5.W))
+    val csr_cmd = Input(UInt(3.W))
+    val csr_in = Input(UInt(1.W))
   })
+
+  val W = 1.U
+  val S = 2.U
+  val C = 3.U
+
+  val mstatus = RegInit(UInt(32.W))
+  val mtvec = RegInit(UInt(32.W))
+  val mie = RegInit(UInt(32.W))
+  val mip = RegInit(UInt(32.W))
+  val mepc = RegInit(UInt(32.W))
+
+
 }
