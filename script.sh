ecaa8bbbca1bf6829a262cd1ab012539ae792da9
Talha Ahmed <talhaahmedashraf@gmail.com>
2022-07-06 01:01:31 +0500

    run compliance by just e.g ./script MUL

--- script.sh
+++ script.sh
@@ -1,19 +1,9 @@
-    #!/bin/bash
-    # Ask the user for their name
-    #echo Which Test you want to run?
-    # read test_name
-    #echo Running $1 ...
+export NUCLEUSRV=$PWD
+FILE=$NUCLEUSRV/test_run_dir/Top_Test/VTop
 
-    
-
-    #S_path=$PWD
-    export NUCLEUSRV=$PWD
-    FILE=$NUCLEUSRV/test_run_dir/Top_Test/VTop
-
-    #export NUCLEUSRV=$SocNow_path/SoCNow/nucleusrv RISCV_TEST="I-ADD-01"
-    TEST=$1
-    cd $NUCLEUSRV/riscv-arch-test
-    make clean TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32im RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop
-    make TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32im RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop | tee Test_result.txt
-    cd ../
-    exec bash
+TEST=$1
+cd $NUCLEUSRV/riscv-arch-test
+make clean TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32im RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop
+make TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32im RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop | tee Test_result.txt
+cd ../
+exec bash
