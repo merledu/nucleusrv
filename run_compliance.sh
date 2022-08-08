6295a648634893f545df097be21fd282cc4d9f12
Talha Ahmed <talhaahmedashraf@gmail.com>
2022-08-09 02:33:01 +0500

    update make and compliance script for bith I and M

--- run_compliance.sh
+++ run_compliance.sh
@@ -2,8 +2,9 @@ export NUCLEUSRV=$PWD
 FILE=$NUCLEUSRV/test_run_dir/Top_Test/VTop
 
 TEST=$1
+ISA=$2
 cd $NUCLEUSRV/riscv-arch-test
-make clean TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32im RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop
-make TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32im RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop | tee Test_result.txt
+make clean TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=$ISA RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop
+make TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=$ISA RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop | tee Test_result.txt
 cd ../
 exec bash
