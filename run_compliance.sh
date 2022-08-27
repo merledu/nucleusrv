703c8c596d4a78bc02d5b52205531ce88310ba1c
Usman <usmanzain95@gmail.com>
2022-08-27 23:06:03 +0500

    add shebang

--- run_compliance.sh
+++ run_compliance.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 export NUCLEUSRV=$PWD
 FILE=$NUCLEUSRV/test_run_dir/Top_Test/VTop
 
 TEST=$1
 cd $NUCLEUSRV/riscv-arch-test
 make clean TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32im RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop
-make TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32im RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop | tee Test_result.txt
+make TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32imc RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop | tee Test_result.txt
 cd ../
-exec bash
