1ddcc0dbf4348999b3670dca988ca8ab5b6e97eb
Usman <usmanzain95@gmail.com>
2022-08-27 23:42:13 +0500

    add toolchain prefix

--- run_compliance.sh
+++ run_compliance.sh
@@ -5,6 +5,6 @@ FILE=$NUCLEUSRV/test_run_dir/Top_Test/VTop
 
 TEST=$1
 cd $NUCLEUSRV/riscv-arch-test
-make clean TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32im RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop
-make TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32imc RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop | tee Test_result.txt
+make clean TARGETDIR=$NUCLEUSRV/riscv-target RISCV_PREFIX=riscv32-unknown-elf-  RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32im RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop
+make TARGETDIR=$NUCLEUSRV/riscv-target RISCV_PREFIX=riscv32-unknown-elf- RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32imc RISCV_TEST=$TEST TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop | tee Test_result.txt
 cd ../
