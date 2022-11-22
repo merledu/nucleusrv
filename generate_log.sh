86d530709f40dc27e51983505c701362d59705e4
Usman <usmanzain95@gmail.com>
2022-11-22 16:15:26 +0500

    not required, trecer is independant

--- generate_log.sh
+++
@@ -1,4 +0,0 @@
-curr_dir=$PWD
-echo "Current directory: $curr_dir"
-sbt "testOnly nucleusrv.components.TopTest -- -DprogramFile=$curr_dir/simulation/machine.hex" > simulation/log.txt
-python3 simulation/extract.py
--- simulation/assembly.s
+++
@@ -1,3 +0,0 @@
-li x3,5
-li x4,6
-add x5,x3,x4
