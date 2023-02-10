2c46fae1f18dfb3fb85d0b2a5e9d7e79dbc7a1ef
talha-ahmed-1 <talhaahmedashraf@gmail.com>
2023-02-10 22:16:49 +0500

    all csrs added

--- src/main/scala/csr/AddressMap.scala
+++ src/main/scala/csr/AddressMap.scala
@@ -8,4 +8,8 @@ object AddressMap {
     val MISA             = "h301".U
     val MHARTID          = "hf14".U
     val MSTATUS          = "h300".U
+    val MTVEC            = "h305".U
+    val MIE              = "h304".U
+    val MCAUSE           = "h342".U
+    val MEPC             = "h341".U
 }
--- src/main/scala/csr/CSRRegFile.scala
+++ src/main/scala/csr/CSRRegFile.scala
@@ -17,22 +17,30 @@ class CSRRegFile extends Module{
     // Registers
     val MISA_REG            = RegInit(0.U(32.W))
     val MHARTID_REG         = RegInit(0.U(32.W))
+    val MCAUSE_REG          = RegInit(0.U(32.W))
+    val MTVEC_REG           = RegInit(0.U(32.W))
+    val MEPC_REG            = RegInit(0.U(32.W))
+    val MIE_REG             = RegInit(0.U(32.W))
     
     // MSTATUS
-    val TW                  = RegInit(0.U(1.W))
-    val MPRV                = RegInit(0.U(1.W))
-    val MPP                 = RegInit(0.U(2.W))
-    val MPIE                = RegInit(0.U(1.W))
-    val MIE                 = RegInit(0.U(1.W))
+    val MSTATUS_TW_REG      = RegInit(0.U(1.W))
+    val MSTATUS_MPRV_REG    = RegInit(0.U(1.W))
+    val MSTATUS_MPP_REG     = RegInit(0.U(2.W))
+    val MSTATUS_MPIE_REG    = RegInit(0.U(1.W))
+    val MSTATUS_MIE_REG     = RegInit(0.U(1.W))
 
     // Hardwired
     MISA_REG                := io.MISA.i_value
     MHARTID_REG             := io.MHARTID.i_value
 
     // Wires
-    val w_data              = Wire(UInt(32.W))
-    val r_data              = Wire(UInt(32.W))
-    val MSTATUS_WIRE        = WireInit(Cat("b0".U(10.W), TW, "b0".U(3.W), MPRV, "b0".U(4.W), MPP, "b0".U(3.W), MPIE, "b0".U(3.W), MIE, "b0".U(3.W)))
+    val w_data                  = Wire(UInt(32.W))
+    val r_data                  = Wire(UInt(32.W))
+    val MSTATUS_WIRE            = WireInit(Cat("b0".U(10.W), MSTATUS_TW_REG, "b0".U(3.W), MSTATUS_MPRV_REG, "b0".U(4.W), MSTATUS_MPP_REG, "b0".U(3.W), MSTATUS_MPIE_REG, "b0".U(3.W), MSTATUS_MIE_REG, "b0".U(3.W)))
+    val MCAUSE_WLRL_WIRE        = WireInit(MCAUSE_REG(30,0))
+    val MCAUSE_INTERRUPT_WIRE   = WireInit(MCAUSE_REG(31))
+    val MTVEC_MODE_WIRE         = WireInit(MTVEC_REG(1,0))
+    val MTVEC_BASE_WIRE         = WireInit(MTVEC_REG(31,2))
 
     val csr_opr = CSROperations()
     /*************************************************/
@@ -44,7 +52,11 @@ class CSRRegFile extends Module{
     val READ_CASES = Array(
         AddressMap.MISA    -> MISA_REG,
         AddressMap.MHARTID -> MHARTID_REG,
-        AddressMap.MSTATUS -> MSTATUS_WIRE
+        AddressMap.MSTATUS -> MSTATUS_WIRE,
+        AddressMap.MCAUSE  -> MCAUSE_REG,
+        AddressMap.MTVEC   -> MTVEC_REG,
+        AddressMap.MEPC    -> MEPC_REG,
+        AddressMap.MIE     -> MIE_REG
     )
 
     r_data := MuxLookup(io.CSR.i_addr, DontCare, READ_CASES)
@@ -67,11 +79,23 @@ class CSRRegFile extends Module{
     when(io.CSR.i_w_en){
         switch(io.CSR.i_addr){
             is(AddressMap.MSTATUS){
-                TW   := w_data(21)
-                MPRV := w_data(17)
-                MPP  := w_data(12,11)
-                MPIE := w_data(7)
-                MIE  := w_data(3)
+                MSTATUS_TW_REG   := w_data(21)
+                MSTATUS_MPRV_REG := w_data(17)
+                MSTATUS_MPP_REG  := w_data(12,11)
+                MSTATUS_MPIE_REG := w_data(7)
+                MSTATUS_MIE_REG  := w_data(3)
+            }
+            is(AddressMap.MCAUSE){
+                MCAUSE_REG       := w_data
+            }
+            is(AddressMap.MTVEC){
+                MTVEC_REG        := w_data
+            }
+            is(AddressMap.MEPC){
+                MEPC_REG         := w_data
+            }
+            is(AddressMap.MIE){
+                MIE_REG          := w_data
             }
         }
     }
