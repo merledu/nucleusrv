c9537a453f0b81db1e5674404b73c68fffca135d
shahzaibk23 <shahzaibceo@gmail.com>
2023-11-06 16:55:53 +0500

    attached error signal to be compatible w. caravan

--- src/main/scala/components/MemIO.scala
+++ src/main/scala/components/MemIO.scala
@@ -8,12 +8,13 @@ import chisel3._
  * It will set these signals up in order to talk to the Host adapter of the relevant bus protocol
  */
 class MemRequestIO extends Bundle {
-  val addrRequest: UInt = Input(UInt(32.W))
-  val dataRequest: UInt = Input(UInt(32.W))
+  val addrRequest   : UInt = Input(UInt(32.W))
+  val dataRequest   : UInt = Input(UInt(32.W))
   val activeByteLane: UInt = Input(UInt(4.W))
-  val isWrite: Bool = Input(Bool())
+  val isWrite       : Bool = Input(Bool())
 }
 
 class MemResponseIO extends Bundle {
   val dataResponse: UInt = Input(UInt(32.W))
+  val error       : Bool = Input(Bool())
 }
