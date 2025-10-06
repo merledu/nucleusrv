0d18a6db71b4f2d44a2b171fc3e7a8540cd4e7b7
kinzafatim <kinzaf54@gmail.com>
2025-10-06 20:43:43 +0500

    New top with caches connections added

---
+++ src/main/scala/components/new_Top.scala
@@ -0,0 +1,117 @@
+package nucleusrv.components
+
+import chisel3._
+import chisel3.stage.ChiselStage
+import nucleusrv.tracer._
+import cachematic._
+
+
+class Top(programFile: Option[String], dataFile: Option[String]) extends Module {
+
+  val io = IO(new Bundle {
+    val pin  = Output(UInt(32.W))
+    val rvfi = new TracerO
+  })
+
+  // Core configuration
+  implicit val config: Configs = Configs(
+    XLEN = 32,
+    M = true,
+    F = true,
+    C = false,
+    Zicsr = true,
+    TRACE = true
+  )
+
+  //  2 NucleusRV core
+  val core0 = Module(new Core())
+  val core1 = Module(new Core())
+  core0.io.stall := false.B
+  core1.io.stall := false.B
+
+  //   caches (16KB each 64B line)
+  val icache0 = Module(new Cache(16384, 64, CacheAccessType.ReadOnly))   // icache for Core0
+  val dcache0 = Module(new Cache(16384, 64, CacheAccessType.ReadWrite))  // dcache for Core0
+
+  val icache1 = Module(new Cache(16384, 64, CacheAccessType.ReadOnly))   // icache for Core1
+  val dcache1 = Module(new Cache(16384, 64, CacheAccessType.ReadWrite))  // dcache for Core1
+
+  //  Shared Bus + Memory system
+  val bus0 = Module(new Bus()) // Core0 bus to imem,dmem
+  val bus1 = Module(new Bus()) // Core1 bus to imem,dmem
+
+  val imem = Module(new SRamTop(programFile))
+  val dmem = Module(new SRamTop(dataFile))
+
+  //  Connect Core0 to dcaches, icache
+  core0.io.imemReq.addr  := icache0.io.addr
+  core0.io.imemRsp.data  := icache0.io.rdata
+  core0.io.imemRsp.valid := icache0.io.valid
+
+  core0.io.dmemReq.addr  := dcache0.io.addr
+  core0.io.dmemReq.wdata := dcache0.io.wdata
+  core0.io.dmemReq.wen   := dcache0.io.wen
+  core0.io.dmemRsp.data  := dcache0.io.rdata
+  core0.io.dmemRsp.valid := dcache0.io.valid
+
+  //  Connect Core1 to dcaches, icache
+
+  core1.io.imemReq.addr  := icache1.io.addr
+  core1.io.imemRsp.data  := icache1.io.rdata
+  core1.io.imemRsp.valid := icache1.io.valid
+
+  core1.io.dmemReq.addr  := dcache1.io.addr
+  core1.io.dmemReq.wdata := dcache1.io.wdata
+  core1.io.dmemReq.wen   := dcache1.io.wen
+  core1.io.dmemRsp.data  := dcache1.io.rdata
+  core1.io.dmemRsp.valid := dcache1.io.valid
+
+  
+  //  Connect caches to bus
+  
+  // Core0 cache to Bus0
+  bus0.io.cpu.addr  := Mux(icache0.io.valid, icache0.io.addr, dcache0.io.addr)
+  bus0.io.cpu.wdata := dcache0.io.wdata
+  bus0.io.cpu.wen   := dcache0.io.wen
+  bus0.io.cpu.ren   := !dcache0.io.wen
+  dcache0.io.rdata  := bus0.io.cpu.rdata
+  icache0.io.rdata  := bus0.io.cpu.rdata
+
+  // Core1 caches to Bus1
+  bus1.io.cpu.addr  := Mux(icache1.io.valid, icache1.io.addr, dcache1.io.addr)
+  bus1.io.cpu.wdata := dcache1.io.wdata
+  bus1.io.cpu.wen   := dcache1.io.wen
+  bus1.io.cpu.ren   := !dcache1.io.wen
+  dcache1.io.rdata  := bus1.io.cpu.rdata
+  icache1.io.rdata  := bus1.io.cpu.rdata
+
+
+  //  Shared Memory connection
+  bus0.io.imem <> imem.io
+  bus0.io.dmem <> dmem.io
+
+  bus1.io.imem <> imem.io
+  bus1.io.dmem <> dmem.io
+
+  io.pin := core0.io.pin | core1.io.pin
+
+  if (config.TRACE) {
+    val tracer = Module(new Tracer)
+    tracer.rvfi_i <> core0.io.rvfi.get
+    io.rvfi <> tracer.rvfi_o
+  }
+}
+
+object NRVDriver {
+  def main(args: Array[String]): Unit = {
+    val IMem = if (args.contains("--imem")) Some(args(args.indexOf("--imem") + 1)) else None
+    val DMem = if (args.contains("--dmem")) Some(args(args.indexOf("--dmem") + 1)) else None
+    new ChiselStage().emitVerilog(
+      new Top(IMem, DMem),
+      if (args.contains("--target-dir")) args.slice(
+        args.indexOf("--target-dir"),
+        args.indexOf("--target-dir") + 2
+      ) else Array()
+    )
+  }
+}
